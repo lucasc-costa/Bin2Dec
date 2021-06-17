44f20f972b24ef18cde82e501fb7e33f8b04f0a7
lucasc-costa <62854155+lucasc-costa@users.noreply.github.com>
2021-06-16 21:12:17 -0300

    finalizando
    criação do js

---
+++ src/functions.js
@@ -0,0 +1,25 @@
+function converter(){
+    var binary=document.getElementById("Binary").value;
+    var sum=0;
+    var scale=testNumber(binary);
+    window.alert(scale)
+    for (let i = 0; i < scale; i++){
+        var element = binary[i]*2**((scale-1)-i);  
+        sum += element;
+    }
+   document.getElementById("Decimal").value=sum;
+}   
+
+function testNumber(number){
+    var scale=0;
+    for (let i = 0; i < number.length; i++) {
+        if (number[i]!=1 && number[i]!=0) {
+            window.alert("Please, enter a binary number")
+            break
+        }else{
+            scale+=1
+        }
+    }
+    return scale;
+
+}
